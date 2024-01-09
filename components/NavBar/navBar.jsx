886bad04377e576cc2decb1fcb3a602663c53e55
Carlos <carloscauantecnico@gmail.com>
2024-01-09 03:38:01 -0300

    Fix navBar

--- components/NavBar/navBar.jsx
+++ components/NavBar/navBar.jsx
@@ -6,7 +6,7 @@ const NavBarContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   justify-content: center;
-  width: 200em;
+  width: 117em;
   margin-top: 2rem;
   background-color: #1f1f1f;
 `;
